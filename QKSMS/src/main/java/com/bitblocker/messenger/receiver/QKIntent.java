26598aa12f6617fd418857c853b1bc6213dd76ad
Dato0011 <dato0011@hotmail.com>
2017-04-02 19:28:24 +0400

    Downgraded minimum sdk version to 21.

---
+++ QKSMS/src/main/java/com/bitblocker/messenger/receiver/QKIntent.java
@@ -0,0 +1,5 @@
+package com.bitblocker.messenger.receiver;
+
+public class QKIntent {
+    public static final String ACTION_ANALYTICS_REPORT = "com.bitblocker.messenger.action.analytics_report";
+}
